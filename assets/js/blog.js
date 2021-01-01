a7d748df7e4540408cd361a025f9f2ceb9347d0d
Ariel Montes <arielmontes1989@gmail.com>
2021-01-01 02:57:53 +0100

    Fix blog code highlight

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,9 +1,14 @@
+hljs.configure({tabReplace: '    ', languages: ['bash', 'python', 'apache']});
+hljs.initHighlightingOnLoad();
+
+/*
 $(document).ready(function() {
 	
-	/* ======= Highlight.js Plugin ======= */ 
-    /* Ref: https://highlightjs.org/usage/ */     
+	// ======= Highlight.js Plugin =======
+    // Ref: https://highlightjs.org/usage/  
     $('pre code').each(function(i, block) {
 	    hljs.highlightBlock(block);
 	 });
 
 });
+*/
