6bcf946d403bf25f9fc62271d52388dd9c7fbc0e
Ariel Montes <arielmontes1989@gmail.com>
2021-01-12 15:37:00 +0100

    aesthetic improvements

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,14 +1,1 @@
-hljs.configure({tabReplace:'    ',languages:['bash','python','apache']});
-hljs.initHighlightingOnLoad();
-
-/*
-$(document).ready(function() {
-	
-	// ======= Highlight.js Plugin =======
-    // Ref: https://highlightjs.org/usage/  
-    $('pre code').each(function(i, block) {
-	    hljs.highlightBlock(block);
-	 });
-
-});
-*/
+hljs.configure({tabReplace:"    ",languages:["bash","python","apache"]}),hljs.initHighlightingOnLoad(),$(document).ready(function(){$("code").each(function(){if($(this).attr("class")){let e=$(this).attr("class").replace("hljs","").replace(" ","");e&&$(this).before('<div class="code-lang non-selectable">'+e+"</div>")}})});
